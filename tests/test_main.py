edec2f72c724198ab57dfc342727e333c1c4ee59
miyatsuki <6367574+miyatsuki@users.noreply.github.com>
2024-07-15 17:55:26 +0900

    fix: code_promptでファイル間の改行がなかった

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 import unittest
 from pathlib import Path
+from tempfile import TemporaryDirectory
+
 from main import create_code_prompt
 
 
@@ -9,22 +11,21 @@ class TestMain(unittest.TestCase):
 
     def test_create_code_prompt(self):
         # テスト用のディレクトリとファイルを設定
-        work_dir = Path('test_dir')
-        work_dir.mkdir(exist_ok=True)
-        
-        file1 = work_dir / 'file1.txt'
-        file1.write_text('Content of file1')
-        
-        file2 = work_dir / 'file2.txt'
-        file2.write_text('Content of file2')
-        
-        selected_files = ['file1.txt', 'file2.txt']
-        
-        expected_output = '```file1.txt\nContent of file1```\n```file2.txt\nContent of file2```'
-        
-        result = create_code_prompt(selected_files, work_dir)
-        
-        self.assertEqual(result.strip(), expected_output)
-        
-        # クリーンアップ
-        work_dir.rmdir()
+        with TemporaryDirectory() as tmp_dir_name:
+            tmp_dir = Path(tmp_dir_name)
+
+            file1 = tmp_dir / "file1.txt"
+            file1.write_text("Content of file1")
+
+            file2 = tmp_dir / "file2.txt"
+            file2.write_text("Content of file2")
+
+            selected_files = ["file1.txt", "file2.txt"]
+
+            expected_output = (
+                "```file1.txt\nContent of file1```\n\n```file2.txt\nContent of file2```"
+            )
+
+            result = create_code_prompt(selected_files, tmp_dir)
+
+        self.assertEqual(expected_output, result.strip())
